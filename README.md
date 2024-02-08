a1e54f1f9db0fc8d2c99b547345dc7b41fce2298
cristinagiacobbe <crigiak@gmail.com>
2024-02-08 23:01:12 +0100

    Complete cards on page (name-image-archetype)

--- README.md
+++ README.md
@@ -10,10 +10,12 @@ Create un nuovo progetto utilizzando Vite e Vue 3 e definite i componenti necess
 Al caricamento della pagina, effettuate una chiama ajax all'API di Yu Gi Oh:
 https://db.ygoprodeck.com/api/v7/cardinfo.php
 con i dati restituiti, stampate una card per ogni carta.
-:saetta: ATTENZIONE:
+
+ATTENZIONE:
 l’api restituisce tutti i risultati in un colpo solo.
 Per evitare attese e/o rallentamenti nelle richieste, potete diminuire il numero di risultati sfruttando i parametri num e offset
 https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0
-:simbolo_addizione_in_grassetto: Bonus:
+
+Bonus:
 Creare un componente loader da visualizzare fintantoché i risultati non sono pronti.
 Documentazione: https://ygoprodeck.com/api-guide/
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,19 +5,22 @@ export default {
     data() {
         return {
             base_api_url: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=20",
-            aliens: []
-        }
+            cards: [],
+
+        };
+
     },
+
     mounted() {
         axios
             .get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=20")
             .then((response) => {
-                console.log(response.data);
-                this.aliens = response.data;
-
-            })
+                console.log(response.data.data);
+                this.cards = response.data.data;
+                console.log(this.cards[0].card_images[0].image_url);
+            });
     }
-}
+};
 </script>
 <template>
     <div class="container">
@@ -25,8 +28,10 @@ export default {
             <h4>Found 39 cards</h4>
         </div>
         <div class="row">
-            <div class="col" v-for="alien in aliens" :key="alien.id + '_alien'">
-                <h4>{{ alien }}</h4>
+            <div class="col-5" v-for="card in cards" :key="card.id + '_alien'">
+                <img :src="card.card_images[0].image_url" alt="">
+                <h4>{{ card.name }}</h4>
+                <h5>{{ card.archetype }}</h5>
             </div>
         </div>
     </div>
@@ -39,8 +44,23 @@ export default {
 }
 
 .col-5 {
-    width: calc(100% / 12 * 5);
+    width: calc(100% / 5);
     background-color: var(--yu-primary);
+    padding: 0.5rem;
+}
+
+img {
+    width: 100%;
+}
+
+h4 {
+    color: var(--yu-light);
+    text-transform: uppercase;
+    text-align: center;
+}
+
+h5 {
+    text-align: center;
 }
 
 .found {
